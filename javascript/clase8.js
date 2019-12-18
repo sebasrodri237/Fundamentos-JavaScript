a2b0738b9cd58f18b25a478339ac7acb934fc4d0
sebasrodri237 <sebasplay2@hotmail.com>
2019-12-18 16:13:33 -0500

    Cuando los objetos se pasan como una referencia, estos se modifican fuera de la función. Para solucionar esto se puede crear un objeto diferente. Esto lo podemos hacer colocando tres puntos antes del nombre. Ej …persona.

---
+++ javascript/clase8.js
@@ -0,0 +1,25 @@
+var sebastian = {
+
+    nombre: 'Sebastian',
+    apellido: 'Rodriguez',
+    edad: 24
+}
+
+var pepe = {
+
+    nombre: 'Pepe',
+    apellido: 'Perez',
+    edad: 45
+}
+
+function birthday(persona){
+
+    return {
+
+        ...persona,
+        edad: persona.edad += 1
+    }
+    
+}
+/*Esta funcion creara un nuevo objeto persona pero modificado
+mientras que el objeto que pasamos por parametro no cambiara */
