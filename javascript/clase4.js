37f06ffdb9da64d7a89aed0e8f282edffb83ee05
sebasrodri237 <sebasplay2@hotmail.com>
2019-12-17 22:49:30 -0500

    Clase 4 sobre variables funciones, invocar y pasar parametros

---
+++ javascript/clase4.js
@@ -0,0 +1,9 @@
+var nombre = 'Sebastian', edad = 24;
+
+function imprimirEdad(name, age){
+
+    console.log(`${name} tiene ${age} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Sacha' , 28);
