2eca992d0904e04967f66ea309e4f5af80aa8972
sebasrodri237 <sebasplay2@hotmail.com>
2019-12-27 16:11:58 -0500

    Generando una secuencia de números, usamos new Array() para crear el arreglo de manera dinámica, y llamamos al método fill para rellenar ese array con ceros y poder luego iterar sobre éste con map()

---
+++ javascript/clase38.js
@@ -0,0 +1,43 @@
+const celeste = document.getElementById('celeste');
+        const violeta = document.getElementById('violeta');
+        const naranja = document.getElementById('naranja');
+        const verde = document.getElementById('verde');
+        const btnEmpezar = document.getElementById('btnEmpezar');
+
+        class Juego{
+
+            constructor(){
+
+                this.inicializar();
+                this.generarSecuencia();
+            }
+
+            inicializar(){
+
+                btnEmpezar.classList.add('hide');/*Aqui le agrego una clase
+                al boton de empezar en css, esta clase tiene display:none
+                para ocultar el boton*/
+                this.nivel = 1;
+                this.colores = {
+
+                    celeste,
+                    violeta,
+                    naranja,
+                    verde
+                }/*Puedo colocarlos asi sin clave, ya que en JavaScript
+                si la clave y el valor son el mismo lo puedo dejar asi*/
+            }
+
+            generarSecuencia(){
+
+                this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random()*4));
+                /*Otra forma de crear un array, rellenarlo y modificarlo*/
+            }
+        }
+
+        function empezarJuego() {
+            
+            window.juego = new Juego()/*Para poder debuggearla, mirando
+            en consola que sucede con esta variable*/
+            var x = 1;
+        }
