e3ef6459706dabaeab9e23c34917711d78e80973
sebasrodri237 <sebasplay2@hotmail.com>
2020-01-01 22:59:10 -0500

    Funciones recursivas, Una función recursiva es básicamente aquella que se llama (o se ejecuta) a sí misma de forma controlada, hasta que sucede una condición base.

---
+++ javascript/funcionesRecursivas.js
@@ -0,0 +1,9 @@
+function divisionEntera(dividendo, divisor) {
+    
+    if (dividendo < divisor) {
+        
+        return 0;
+    }
+
+    return 1 + divisionEntera(dividendo - divisor, divisor);
+}
