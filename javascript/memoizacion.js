6b8b90161f01a76a8ad4fa979eab0d31bd928427
sebasrodri237 <sebasplay2@hotmail.com>
2020-01-01 23:22:24 -0500

    Memoización: ahorrando cómputo, es una técnica de programación que nos permite ahorrar cómputo o procesamiento en JavaScript

---
+++ javascript/memoizacion.js
@@ -0,0 +1,22 @@
+function factorial(n) {
+    
+    if (!this.cache) {
+        
+        this.cache = {}
+    }/*Pregunta si existe una cache de esta funcion */
+
+    debugger
+    if (this.cache[n]) {
+        
+        return this.cache[n];
+    }
+
+    if (n === 1) {
+        
+        return 1;
+    }
+
+    this.cache[n] =  n * factorial(n-1);
+    debugger
+    return this.cache[n];
+}
